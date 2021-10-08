01a3346d67628a0eddb0f19b15217161b160f52c
Hin-Tak Leung <htl10@users.sourceforge.net>
2021-10-08 02:07:55 +0100

    Fixes -Wmisleading-indentation, after clarifying the previous -Wdangling-else
    No actual code change, at all. Just white-space differences.

--- src/getopt.h
+++ src/getopt.h
@@ -25,33 +25,33 @@ public:
 				return(END);
 			}
 		}
-			m_optopt = c = m_argv[m_optind][m_sp];
-			if (c == ':' || (cp = strchr(m_opts, c)) == nullptr) {
-				if (m_argv[m_optind][++m_sp] == '\0') {
-					m_optind++;
-					m_sp = 1;
-				}
-				return('?');
-			}
-			if (*++cp == ':') {
-				if (m_argv[m_optind][m_sp + 1] != '\0')
-					m_optarg = &m_argv[m_optind++][m_sp + 1];
-				else if (++m_optind >= m_argc) {
-					m_sp = 1;
-					return('?');
-				}
-				else
-					m_optarg = m_argv[m_optind++];
+		m_optopt = c = m_argv[m_optind][m_sp];
+		if (c == ':' || (cp = strchr(m_opts, c)) == nullptr) {
+			if (m_argv[m_optind][++m_sp] == '\0') {
+				m_optind++;
 				m_sp = 1;
 			}
-			else {
-				if (m_argv[m_optind][++m_sp] == '\0') {
-					m_sp = 1;
-					m_optind++;
-				}
-				m_optarg = nullptr;
+			return('?');
+		}
+		if (*++cp == ':') {
+			if (m_argv[m_optind][m_sp + 1] != '\0')
+				m_optarg = &m_argv[m_optind++][m_sp + 1];
+			else if (++m_optind >= m_argc) {
+				m_sp = 1;
+				return('?');
 			}
-			return(c);
+			else
+				m_optarg = m_argv[m_optind++];
+			m_sp = 1;
+		}
+		else {
+			if (m_argv[m_optind][++m_sp] == '\0') {
+				m_sp = 1;
+				m_optind++;
+			}
+			m_optarg = nullptr;
+		}
+		return(c);
 	}; 
 	char  GetOptionOption() {return m_optopt;};
 	char* GetOptionArgument() {return m_optarg;};
