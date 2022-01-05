630cad5d6fc0331b7c2d472409d7ba2ff20ccacf
Paul Linnerud <paulli@microsoft.com>
2022-01-05 13:45:39 -0800

    Update setup.py
    version = '0.0.1.2'

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ extension = Extension(
 
 setup(
     name="vttcompilepy", 
-    version= '0.0.1.1b1',
+    version= '0.0.1.2',
     description="Python extension for Visual TrueType font compile. ",
     long_description=long_description,
     long_description_content_type='text/markdown',
